0c2587a995ff07faf7824b9c7caf7fd8c935f329
austin c <ac6404@gmail.com>
2021-12-28 17:14:20 -0600

    refactor upload code

--- bii_multi_pheno_cam/UploadService.swift
+++ bii_multi_pheno_cam/UploadService.swift
@@ -15,26 +15,26 @@ import SwiftUI
 public class UploadService {
     
     let dataService: DataService
-    var sessionId: UUID?
-    var siteId: Int?
-    var blockId: Int?
-    var sessionStart: Date?
-    var sessionStop: Date?
-    var creationDate: Date?
-    var url: URL?
+//    var sessionId: UUID?
+//    var siteId: Int?
+//    var blockId: Int?
+//    var sessionStart: Date?
+//    var sessionStop: Date?
+//    var creationDate: Date?
+//    var url: URL?
     @Published var isUploading: Bool
     @Published var statusMessage: String
     
     
     init() {
         dataService = DataService()
-        sessionId = nil
-        siteId = nil
-        blockId = nil
-        sessionStart = nil
-        sessionStop = nil
-        creationDate = nil
-        url = nil
+//        sessionId = nil
+//        siteId = nil
+//        blockId = nil
+//        sessionStart = nil
+//        sessionStop = nil
+//        creationDate = nil
+//        url = nil
         isUploading = false
         statusMessage = ""
     }
@@ -44,22 +44,60 @@ public class UploadService {
         self.statusMessage = "Uploading..."
         let realm = try! Realm()
         let storage = Storage.storage() // this is a reference to firebase cloud storage service
-        let db = Firestore.firestore() // this is our reference to the firestore database
+        let db = Firestore.firestore() // this is our reference to the firestore NoSQL database
         let reference = storage.reference()
         let imagesRef = reference.child("images")
         
+        var imageUrl: URL?
+        var creationDate: Date?
+        
+        
+        // This next chunk groups the photo references from multiple sessions into 1 array. It also flattens the session metadata using multiple dictionaries. It maps array indices matching future fetchAssets results to keys which are mapped to sessionData instances. Refactored from having to run all the remaining upload code for each individual session (multiple db and network calls in a nested for loop -- ick!). Still, I can't tell if this is clever or just wrong. Nixing the extra for loop also made it possible to use the group dispatch easily and have a clear way to tell when all the async calls where complete. If this is ruining your day right now, I'm sorry!
+        
+        // For Future refactor: Maybe redesigning the data models in the Realm db could make this uneccessary.
+        
+        var counter = 0 // This becomes dict keys that match the array indices of images in fetchResults
+        var keyValue = 0 // Each keyvalue (0, 1, 2, ...n) corresponds to 1 set of session metadata
+        var keyMap = [Int:Int]() // maps the above two values to each other. ie Which photos at what index are associated with what metadata
+        var sessionDataDictionary = [Int:sessionData]() // Maps a numeric key to each session metadata
+        var combinedPhotoReferences = [String]()
+        
         let sessions = realm.objects(PhotoCaptureSession.self).filter("sessionId IN %@", sessionIds)
         
-        //TODO: Simple refactor here to reduce fetch asset calls inside this for loop
         for session in sessions {
-            self.sessionId = UUID()
-            print(self.sessionId)
-            self.siteId = session.siteId
-            self.blockId = session.blockId
-            self.sessionStart = session.sessionStart
-            self.sessionStop = session.sessionStop
             
-            var fetchResults = PHAsset.fetchAssets(withLocalIdentifiers: Array(session.photoReferences), options: nil)
+            combinedPhotoReferences += Array(session.photoReferences)
+            
+            for index in counter..<counter + combinedPhotoReferences.count {
+                keyMap[index] = keyValue
+            }
+            
+            // Realm DB identifiers are type ObjecetId which is difficult to work with so we generate a new uuid
+            let newSessionId = UUID()
+            let data = sessionData(
+                sessionId: newSessionId.uuidString,
+                siteId: session.siteId,
+                blockId: session.blockId,
+                sessionStart: session.sessionStart,
+                sessionStop: session.sessionStop
+            )
+            
+            sessionDataDictionary[keyValue] = data
+            
+            counter += combinedPhotoReferences.count
+            keyValue += 1
+
+        }
+        
+//        for session in sessions {
+//            self.sessionId = UUID()
+//            print(self.sessionId)
+//            self.siteId = session.siteId
+//            self.blockId = session.blockId
+//            self.sessionStart = session.sessionStart
+//            self.sessionStop = session.sessionStop
+            
+            var fetchResults = PHAsset.fetchAssets(withLocalIdentifiers: combinedPhotoReferences, options: nil)
 
             if fetchResults.count > 0 {
 
@@ -68,7 +106,7 @@ public class UploadService {
                 for i in 0..<fetchResults.count {
                     
                     // pull metadata from the PHAssets
-                    self.creationDate = fetchResults.object(at: i).creationDate
+                    creationDate = fetchResults.object(at: i).creationDate
                     
                     // Use local identifier as filename for jpeg. Trim repeating sequences after "/"
                     let idString = fetchResults.object(at: i).localIdentifier.split(separator: "/")
@@ -93,16 +131,16 @@ public class UploadService {
                                           return
                                         }
                                         //successfully got resource url
-                                        self.url = url
+                                        imageUrl = url
                                         var ref: DocumentReference? = nil
                                         ref = db.collection("images").addDocument(data: [
-                                            "siteId" : self.siteId,
-                                            "blockId" : self.blockId,
-                                            "sessionId" : self.sessionId!.uuidString,
-                                            "sessionStart" : Timestamp(date: self.sessionStart!),
-                                            "sessionStop" : Timestamp(date: self.sessionStop!),
-                                            "creationDate" : Timestamp(date: self.creationDate!),
-                                            "url" : self.url!.absoluteString
+                                            "siteId" : sessionDataDictionary[keyMap[i]!]!.siteId,
+                                            "blockId" : sessionDataDictionary[keyMap[i]!]!.blockId,
+                                            "sessionId" : sessionDataDictionary[keyMap[i]!]!.sessionId,
+                                            "sessionStart" : Timestamp(date: sessionDataDictionary[keyMap[i]!]!.sessionStart),
+                                            "sessionStop" : Timestamp(date: sessionDataDictionary[keyMap[i]!]!.sessionStop),
+                                            "creationDate" : Timestamp(date: creationDate!),
+                                            "url" : imageUrl!.absoluteString
                                         ]) { err in
                                             if let err = err {
                                                 //TODO: Error writing to firestore
@@ -135,7 +173,15 @@ public class UploadService {
                 self.isUploading = false
                 self.statusMessage = "Error: Unable to find PHAsset with matching local identifier"
             }
-        }
+//        }
     }
+}
+
+struct sessionData {
+    var sessionId: String
+    var siteId: Int
+    var blockId: Int
+    var sessionStart: Date
+    var sessionStop: Date
 }
 
